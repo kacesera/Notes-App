4e155cfd5eb3b29b55df0e45da9f793ece4f8b0c
Ben <ben.lynch1931@gmail.com>
2020-11-09 15:03:37 +0000

    Created instanceMatcher and equalMatcher for Asparagus Test Framework.

---
+++ spec/note-model-spec.js
@@ -0,0 +1,40 @@
+// expect(<INPUT>).toEqual(<RETURN VALUE>)
+// expect(<instance>).toBeA(<class>)
+
+
+class Asparagus {
+
+  constructor(argument) {
+    this.argument = argument;
+  }
+
+  toEqual(expected) {
+    return this._equalMatcher(expected);
+  }
+
+  toBeAnInstanceOf(className) {
+    return this._instanceTypeMatcher(className);
+  }
+
+  toBeAn(className) {
+    return this._instanceTypeMatcher(className);
+  }
+
+  toBeA(className) {
+    return this._instanceTypeMatcher(className);
+  }
+
+
+  _instanceTypeMatcher(className) {
+    return this.argument instanceof className;
+
+  }
+
+  _equalMatcher(expected) {
+    return this.argument === expected;
+  }
+}
+
+function expect(argument) {
+  return new Asparagus(argument);
+}
