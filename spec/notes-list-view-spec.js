61fd46fcc8f4299dd808dc565901ed185c616505
KC Thomas <thisiskcthomas@gmail.com>
2020-11-10 16:29:26 +0000

    Tested that #renderListHTML will return an empty list, and refactored test specfor consistency/readability.

--- spec/notes-list-view-spec.js
+++ spec/notes-list-view-spec.js
@@ -1,26 +1,22 @@
 var test = new Asparagus();
-
 var note1 = {
-  showMessage: function () {
-    return 'First note.';
-  }
+  showMessage: () => { return 'First note.'; }
 }
-
 var note2 = {
-  showMessage: function () {
-    return 'Second note.';
-  }
+  showMessage: () => { return 'Second note.'; }
 }
-
 var notesListModel = {
-  getAllNotes: function () {
-    return [note1, note2];
-  }
+  getAllNotes: () => { return [note1, note2]; }
 };
-
+var noNotesListModel = {
+  getAllNotes: () => { return []; }
+}
 var listHTML = "<ul><li><div>First note.</div></li><li><div>Second note.</div></li></ul>"
-
+var emptyListHTML = "<ul></ul>"
 var notesListView = new NotesListView(notesListModel);
+var noNotesListView = new NotesListView(noNotesListModel);
+
+// --------
 
 test.it("notesListView stores a NotesListModel upon instantiation", () => (
   test.expect(notesListView.getList()).toEqual(notesListModel)
@@ -28,4 +24,8 @@ test.it("notesListView stores a NotesListModel upon instantiation", () => (
 
 test.it("notesListView.renderListHTML() creates an html list with both notes", () => (
   test.expect(notesListView.renderListHTML()).toEqual(listHTML)
-))
+));
+
+test.it("creates an empty list if there are no notes", () => (
+  test.expect(noNotesListView.renderListHTML()).toEqual(emptyListHTML)
+));
